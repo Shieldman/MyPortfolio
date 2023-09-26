cb6691c661751c04625ee2a3817acd26a65b184f
Shieldman <arovirave@gmail.com>
2023-09-26 15:09:18 +0200

    Creation of the basic pages and the navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className='navigation-bar'>
+        <li className='navigation-button'><NavLink to='/'>Home</NavLink></li>
+        <li className='navigation-button'><NavLink to='/projects'>Projects</NavLink></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
